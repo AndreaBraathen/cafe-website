21b3cabe4ae32dbdc25bb6cb0323be5d12ba4405
ANDREA BRAATHEN <andbraa@msn.com>
2023-04-22 17:33:58 +0200

    Created document for categories

---
+++ _studio/schemas/documents/category.js
@@ -0,0 +1,12 @@
+export default {
+   title: 'Category',
+   name: 'category',
+   type: 'document',
+   fields: [
+      {
+         title: 'Type of Food',
+         name: 'typeOfFood',
+         type: 'string',
+      }
+   ]
+}
