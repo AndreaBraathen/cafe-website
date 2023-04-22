dae2afed9068962c9ffff2b93bd0e0bcde9b7d95
ANDREA BRAATHEN <andbraa@msn.com>
2023-04-22 17:34:42 +0200

    Imported the Sanity documents into schemas.js

---
+++ _studio/schemas/schemas.js
@@ -0,0 +1,13 @@
+import menuItem from "./documents/menuItem.js";
+import category from "./documents/category.js";
+import allergen from "./documents/allergen.js";
+import information from "./documents/information.js";
+import location from "./documents/location.js";
+
+export default [
+	menuItem,
+	category,
+	allergen,
+	information,
+	location,
+]
