88dba0a1671dcd0c1d8fc14426c062bcb92eb58f
ANDREA BRAATHEN <andbraa@msn.com>
2023-04-22 17:34:14 +0200

    Created document for allergies

---
+++ _studio/schemas/documents/allergen.js
@@ -0,0 +1,12 @@
+export default {
+	title: 'Allergen',
+	name: 'allergen',
+	type: 'document',
+	fields: [
+		{
+			title: 'Type of Allergen',
+			name: 'typeOfAllergen',
+			type: 'string',
+		}
+	]
+}
