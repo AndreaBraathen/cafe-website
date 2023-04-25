e59ca26f1f7c01c8bf50c5e1a2847c0b9168f0ce
Andrea Bråthen <112381109+AndreaBraathen@users.noreply.github.com>
2023-04-25 12:22:44 +0200

    Feature/sanity document edit (#1)
    * Corrected an writing error
    
    * Created documents for different product-categories
    
    * Used the Structure Builder API
    
    * Commented out some of the documents
    
    * Added elements to the settings-document
    
    * Commented source for the code

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,13 +1,30 @@
 import menuItem from "./documents/menuItem.js";
 import category from "./documents/category.js";
 import allergen from "./documents/allergen.js";
-import information from "./documents/information.js";
-import location from "./documents/location.js";
+// import information from "./documents/information.js";
+// import location from "./documents/location.js";
+// import babyFood from "./documents/babyFood.js";
+import coldDrink from "./documents/coldDrink.js";
+import hotDrink from "./documents/hotDrink.js";
+import food from "./documents/food.js"
+// import salad from "./documents/salad.js";
+// import sandwich from "./documents/sandwich.js";
+// import sweet from "./documents/sweet.js";
+import settings from "./documents/settings.js";
 
 export default [
 	menuItem,
 	category,
 	allergen,
-	information,
-	location,
+	// information,
+	// location,
+	// babyFood,
+	coldDrink,
+	hotDrink,
+	food,
+	// salad,
+	// sandwich,
+	// sweet,
+	settings
+
 ]
---
+++ _studio/structure/cafe.js
@@ -0,0 +1,60 @@
+export default Structure => {
+	const { divider, editor, list, listItem, documentTypeList, documentTypeListItem } = Structure
+
+	// How Sanity is grouped
+	// https://www.sanity.io/docs/manually-group-items-in-a-pane
+
+	return list()
+			.title('Cafe Content')
+			.showIcons(false)
+			.items([
+				listItem()
+				.title('Menu Item')
+				.child(
+					list()
+					.showIcons(false)
+					.title('Menu')
+					.items([
+						listItem()
+							.title('Hot Drink')
+							.child(
+								documentTypeList('hotDrink'),
+							),
+						listItem()
+							.title('Cold Drink')
+							.child(
+								documentTypeList('coldDrink'),
+							),
+						listItem()
+							.title('Food')
+							.child(
+								documentTypeList('food'),
+							),    	
+					])
+				),
+
+				divider(),
+
+				documentTypeListItem('category'),
+				documentTypeListItem('allergen'),
+			])
+
+
+				
+			// .items([
+			// 	documentTypeListItem('menuItem'),
+
+			// 	divider(),
+
+			// 	documentTypeListItem('babyFood'),
+			// 	documentTypeListItem('coldDrink'),
+			// 	documentTypeListItem('hotDrink'),
+			// 	documentTypeListItem('salad'),
+			// 	documentTypeListItem('sandwich'),
+			// 	documentTypeListItem('sweet'),
+
+			// ])
+			
+			
+
+}
