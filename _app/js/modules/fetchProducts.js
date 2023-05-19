e789b806684f74df31f1b42060f52cad72dce299
ANDREA BRAATHEN <andbraa@msn.com>
2023-05-19 22:48:12 +0200

    Fetched ingredients, and added to renderMenu.js

--- _app/js/modules/fetchProducts.js
+++ _app/js/modules/fetchProducts.js
@@ -4,6 +4,7 @@ import { sanity } from "../sanity.js";
 		const query = `*[_type in ["food", "hotDrink", "coldDrink"]] {
 			name,
 			description,
+			ingredients,
 			'mayContainTracesOf': mayContainTracesOf[]->typeOfAllergen,
 			'category': category[]->typeOfFood,
 			'allergens': allergens[]->typeOfAllergen,
