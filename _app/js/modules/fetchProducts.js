d46f76532e74dcbabfe3eef830f6bf67520c5eed
ANDREA BRAATHEN <andbraa@msn.com>
2023-05-19 16:20:36 +0200

    Divided fetching, and render into two js-files

---
+++ _app/js/modules/fetchProducts.js
@@ -0,0 +1,19 @@
+import { sanity } from "../sanity.js";
+
+	export default async function fetchProducts() {
+		const query = `*[_type in ["food", "hotDrink", "coldDrink"]] {
+			name,
+			description,
+			'mayContainTracesOf': mayContainTracesOf[]->typeOfAllergen,
+			'category': category[]->typeOfFood,
+			'allergens': allergens[]->typeOfAllergen,
+			price,
+			_id,
+			'image': image.asset->url
+		 }`;
+	
+		const products = await sanity.fetch(query);
+		// console.log("fetchedProducts:", products)
+		return products;
+
+}
