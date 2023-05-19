2e458c4e6ea5094019b3738dbb9d15c288f28c93
ANDREA BRAATHEN <andbraa@msn.com>
2023-05-19 16:22:55 +0200

    Imported productcard.js and updated main.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,6 +1,11 @@
 import navigation from "./modules/navigation.js";
-import products from "./modules/products.js";
+import fetchProducts from "./modules/fetchProducts.js";
+import renderMenu from "./modules/renderMenu.js";
+import productCard from "./modules/productCard.js";
+
+const fetchedProducts = await fetchProducts(); 
 
 navigation();
-products();
+renderMenu(fetchedProducts);
+productCard(fetchedProducts);
 
