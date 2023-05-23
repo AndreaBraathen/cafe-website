88dd0762cb63ddd8fc5c2486099ae19371ddceba
ANDREA BRAATHEN <andbraa@msn.com>
2023-05-23 13:27:35 +0200

    Imported fetchInformation

--- _app/js/main.js
+++ _app/js/main.js
@@ -2,10 +2,12 @@ import navigation from "./modules/navigation.js";
 import fetchProducts from "./modules/fetchProducts.js";
 import renderMenu from "./modules/renderMenu.js";
 import productCard from "./modules/productCard.js";
+import fetchInformation from "./modules/fetchInformation.js";
 
 const fetchedProducts = await fetchProducts(); 
 
 navigation();
 renderMenu(fetchedProducts);
 productCard(fetchedProducts);
+fetchInformation();
 
